db4f7c9764dc4189d6013225c9e82628fd039b5d
programmer-adil <adc24h@gmail.com>
2022-03-01 13:48:58 +0600

    1st step done

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const allPhone = () => {
+    const searchValue = document.getElementById("search-input").value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displyAllPhone(data.data));
+}
+
+const displyAllPhone = (phones) => {
+    for (const phone of phones) {
+        console.log(phone);
+        const parentDiv = document.getElementById("search-result");
+        div = document.createElement('div');
+        div.className = "col-md-4";
+        div.innerHTML = `<div id="search-result-card" class="search-result-card p-4">
+                            <img class="w-75 mb-4" id="phone-img" src='${phone.image}' alt="">
+                            <h5 class="phone-name">Name: ${phone.phone_name}</h5>
+                            <h6 class="brand-name">Brand Name: ${phone.brand}</h6>
+                        </div>`;
+        parentDiv.appendChild(div)
+    }
+}
