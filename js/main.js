c01fc799e90ee95f2068117c1a0a81998fc91087
programmer-adil <adc24h@gmail.com>
2022-03-02 21:07:57 +0600

    Erorr handaling done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+// call API and handle event when button has clicked
 const allPhone = () => {
+    document.getElementById("search-result").innerHTML = "";
+    document.getElementById("details-card").innerHTML = "";
     const searchValue = document.getElementById("search-input").value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
@@ -6,9 +9,17 @@ const allPhone = () => {
         .then((data) => displyAllPhone(data.data));
 }
 
+// Disply all phone function
 const displyAllPhone = (phones) => {
+    if (phones == false) {
+        document.getElementById("details-card").innerHTML = "";
+        const parantH1 = document.getElementById("search-result");
+        h1 = document.createElement('h1');
+        h1.className = "text-center";
+        h1.innerText = `No data found!`
+        parantH1.appendChild(h1);
+    }
     for (const phone of phones) {
-        console.log(phone);
         if (phones.indexOf(phone) == 20) {
             break;
         }
@@ -28,15 +39,15 @@ const displyAllPhone = (phones) => {
     }
 }
 
+// call API and handle event when button has clicked
 const details = (slug) => {
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
     fetch(url)
         .then((res) => res.json())
         .then((data) => displyDetails(data.data));
 }
-
+// Disply all phone details function
 const displyDetails = (info) => {
-    console.log(info);
     document.getElementById("details-card").innerHTML = `
                 <div class="col-md-6 p-4 d-flex justify-content-center">
                     <img class="w-100 mx-auto" src="${info.image}" alt="">
@@ -45,7 +56,7 @@ const displyDetails = (info) => {
                     <h2 class="text-start specification-title">Specification</h2>
                     <p>Brand Name: ${info.brand}</p>
                     <p>Modle Name: ${info.name}</p>
-                    <p>Release Date: ${info.releaseDate}</p>
+                    <p>Release Date: ${info.releaseDate ? info.releaseDate : 'Release date not found'}</p>
                     <p>Storage: ${info.mainFeatures.storage}</p>
                     <p>Display Size: ${info.mainFeatures.displaySize}</p>
                     <p>Sensors: ${info.mainFeatures.sensors}</p>
