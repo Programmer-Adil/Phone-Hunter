b8fa8488e4d0707e1155f66d508b203d09261585
programmer-adil <adc24h@gmail.com>
2022-03-02 12:42:53 +0600

    Details Btn added

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const allPhone = () => {
 
 const displyAllPhone = (phones) => {
     for (const phone of phones) {
+        console.log(phone);
         if (phones.indexOf(phone) == 20) {
             break;
         }
@@ -19,10 +20,39 @@ const displyAllPhone = (phones) => {
                                 <img class="w-75 mb-4" id="phone-img" src='${phone.image}' alt="">
                                 <h5 class="phone-name">Name: ${phone.phone_name}</h5>
                                 <h6 class="brand-name">Brand Name: ${phone.brand}</h6>
-                                <button class="my-btn btn-details">Details</button>
+                                <button onclick="details('${phone.slug}')" class="my-btn btn-details">Details</button>
                             </div>`;
             parentDiv.appendChild(div);
         }
 
     }
+}
+
+const details = (slug) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displyDetails(data.data));
+}
+
+const displyDetails = (info) => {
+    console.log(info);
+    document.getElementById("details-card").innerHTML = `
+                <div class="col-md-6 p-4 d-flex justify-content-center">
+                    <img class="w-100 mx-auto" src="${info.image}" alt="">
+                </div>
+                <div class="col-md-6 p-4">
+                    <h2 class="text-start specification-title">Specification</h2>
+                    <p>Brand Name: ${info.brand}</p>
+                    <p>Modle Name: ${info.name}</p>
+                    <p>Release Date: ${info.releaseDate}</p>
+                    <p>Storage: ${info.mainFeatures.storage}</p>
+                    <p>Display Size: ${info.mainFeatures.displaySize}</p>
+                    <p>Sensors: ${info.mainFeatures.sensors}</p>
+                    <p>Chip Set: ${info.mainFeatures.chipSet}</p>
+                    <p>WLAN: ${info.others.WLAN}</p>
+                    <p>Bluetooth: ${info.others.Bluetooth}</p>
+                    <p>GPS: ${info.others.GPS}</p>
+                    <p>NFC: ${info.others.NFC}</p>
+                </div>`
 }
